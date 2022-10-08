f9b737764b6500803619194ed2cd23535532ea1f
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2022-10-08 15:07:27 +0200

    :sparkles: Add Cursor Anim

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function cursor(e) {
     mouseCursor.style.top = e.pageY + 'px';
     mouseCursor.style.left = e.pageX + 'px';
 }
+
+var className = "inverted";
+var className2 = "bg";
+var scrollTrigger = 1000;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger) {
+    document.getElementsByClassName("cursor")[0].classList.add(className);
+    document.getElementsByClassName("cursor")[0].classList.add(className2);
+  } else {
+    document.getElementsByClassName("cursor")[0].classList.remove(className);
+    document.getElementsByClassName("cursor")[0].classList.remove(className2);
+  }
+};
